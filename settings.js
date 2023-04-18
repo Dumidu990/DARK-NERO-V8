7e6d0943dc8335956664a9222cbc2b6abdbecebd
QUEEN NICKY <122675835+QUEENNICKY@users.noreply.github.com>
2023-04-18 06:35:30 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -27,7 +27,7 @@ global.socialm = "GitHub: Dark" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.session = " " //session Id
+global.session = "Y0FoS1FNWG0=" //session Id
 global.ownernomer = "94742443114" //ur number
 global.lang = "EN" //Select Language "EN" to English "SI" to sinhala 
 global.premium = ['94742443114'] //ur premium number
